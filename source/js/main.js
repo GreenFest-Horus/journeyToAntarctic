fa90e39b5ad82ab76ba47f2a9919253be6a8ebee
GreenFest-Horus <103495357+GreenFest-Horus@users.noreply.github.com>
2023-04-05 20:53:03 +0300

    добавляет адаптацию каталога, форму и карту

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {initMenu} from './modules/menu/init-menu';
+import {observer, map} from './modules/map/init-map';
 
 // ---------------------------------
 
@@ -23,6 +24,7 @@ window.addEventListener('DOMContentLoaded', () => {
     window.form = form;
     form.init();
     initMenu();
+    observer.observe(map);
   });
 });
 
