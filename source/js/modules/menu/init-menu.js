a4eda1dc140cde8254663098d0a4f1361d3c4a5d
GreenFest-Horus <103495357+GreenFest-Horus@users.noreply.github.com>
2023-04-06 22:06:41 +0300

    добавляет изменения после проверки пп

--- source/js/modules/menu/init-menu.js
+++ source/js/modules/menu/init-menu.js
@@ -1,34 +1,35 @@
-const menuButtons = document.querySelectorAll('[data-menu-button]');
-const menuParent = document.querySelector('[data-menu]');
+const burgerMenu = document.querySelector('[data-burger-menu]');
+const burgerButton = document.querySelector('[data-burger-button]');
+const burgerMenuClosers = document.querySelectorAll('[data-burger-close]');
+let viewportWidth = window.innerWidth;
 
-const toggleMenu = () => {
-  menuParent.classList.toggle('active');
-  document.body.classList.toggle('scroll-lock');
-};
+function openMenu() {
+  burgerMenu.classList.add('active');
+  document.body.classList.add('scroll-lock');
+}
 
-const onOverlayClick = (event) => {
-  if (event.target.closest('[data-menu]')) {
-    return;
+function closeMenu() {
+  burgerMenu.classList.remove('active');
+  document.body.classList.remove('scroll-lock');
+}
+
+function initBurgerMenu() {
+  if (burgerMenu.classList.contains('active')) {
+    closeMenu();
+  } else {
+    openMenu();
   }
+}
 
-  document.body.removeEventListener('click', onOverlayClick, true);
-  toggleMenu();
-};
+burgerButton.addEventListener('click', initBurgerMenu);
+burgerMenuClosers.forEach((closer) => closer.addEventListener('click', closeMenu));
 
-const onMenuToggle = () => {
-  if (menuParent.classList.contains('active')) {
-    document.body.removeEventListener('click', onOverlayClick, true);
-    toggleMenu();
-
-    return;
+window.addEventListener('resize', () => {
+  viewportWidth = window.innerWidth;
+  if (viewportWidth >= 768) {
+    if (burgerMenu.classList.contains('active')) {
+      burgerMenu.classList.remove('active');
+      document.body.classList.remove('scroll-lock');
+    }
   }
-
-  document.body.addEventListener('click', onOverlayClick, true);
-  toggleMenu();
-};
-
-const initMenu = () => {
-  menuButtons.forEach((button) => button.addEventListener('click', onMenuToggle));
-};
-
-export {initMenu};
+});
